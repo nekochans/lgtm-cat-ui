05d240fa95c4db1d839826ea3f646eb0f5503bad
keitakn <keita.koga.work@gmail.com>
2022-05-03 13:23:31 +0900

    :art: #1 webpack5でStorybookが動作するように変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,18 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  core: {
+    builder: 'webpack5',
+  },
 }
