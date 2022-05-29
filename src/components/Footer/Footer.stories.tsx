b269ec575f84f22d8c7f52d9b26c95db55727b65
keitakn <keita.koga.work@gmail.com>
2022-05-29 11:11:25 +0900

    :sparkles: #25 Footer用のComponentを追加

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,12 @@
+import { Footer } from './';
+
+import type { ComponentStoryObj, Meta } from '@storybook/react';
+
+export default {
+  title: 'src/components/Footer/Footer.tsx',
+  component: Footer,
+} as Meta<typeof Footer>;
+
+type Story = ComponentStoryObj<typeof Footer>;
+
+export const Default: Story = {};
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import '@fontsource/roboto';
+
+const StyledFooter = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const UpperSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: flex-start;
+  padding: 10px 605px 20px;
+  background: #fffcf6;
+`;
+
+const UpperSectionText = styled.div`
+  width: 230px;
+  height: 28px;
+  font-family: Roboto;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  color: #43281e;
+  text-align: center;
+  text-decoration-line: underline;
+`;
+
+const LowerSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 644px;
+  background: #f2ebdf;
+`;
+
+const LowerSectionText = styled.div`
+  width: 152px;
+  height: 28px;
+  font-family: Roboto;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  color: #43281e;
+`;
+
+export const Footer: React.FC = () => (
+  <StyledFooter>
+    <UpperSection>
+      {/* eslint-disable no-irregular-whitespace */}
+      <UpperSectionText>利用規約　/　プライバシーポリシー</UpperSectionText>
+    </UpperSection>
+    <LowerSection>
+      <LowerSectionText>Copyright (c) nekochans</LowerSectionText>
+    </LowerSection>
+  </StyledFooter>
+);
