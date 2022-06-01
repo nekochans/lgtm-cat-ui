918890333d945fdd46de2f88bbdcb1d9cef15bba
keitakn <keita.koga.work@gmail.com>
2022-06-01 22:37:27 +0900

    :art: #32 デザイン調整が入ったので適応

--- src/components/GitHubLoginButton/GitHubLoginButton.tsx
+++ src/components/GitHubLoginButton/GitHubLoginButton.tsx
@@ -3,14 +3,14 @@ import { FaGithub } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const StyledGitHubLoginButton = styled.button`
-  position: absolute;
-  top: 20px;
-  left: 20px;
   display: flex;
+  flex: none;
   flex-direction: row;
+  flex-grow: 0;
   gap: 10px;
   align-items: center;
-  width: 117px;
+  order: 0;
+  width: 116px;
   height: 36px;
   padding: 4px 20px;
   background: #eb7c06;
@@ -22,18 +22,29 @@ const Text = styled.div`
   flex-grow: 0;
   order: 1;
   width: 40px;
-  height: 28px;
+  height: 16px;
   font-family: Roboto, sans-serif;
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
-  line-height: 28px;
-  color: #f2ede4;
+  line-height: 16px;
+  color: #f2ebdf;
 `;
+
+const faGithubStyle = {
+  fontStyle: 'normal',
+  fontWeight: 400,
+  width: '26px',
+  height: '26px',
+  color: '#f2ebdf',
+  flex: 'none',
+  order: 1,
+  flexGrow: 0,
+};
 
 export const GitHubLoginButton: React.FC = () => (
   <StyledGitHubLoginButton>
-    <FaGithub style={{ width: '27px', height: '26.33px', color: '#fff' }} />
+    <FaGithub style={faGithubStyle} />
     <Text>Login</Text>
   </StyledGitHubLoginButton>
 );
