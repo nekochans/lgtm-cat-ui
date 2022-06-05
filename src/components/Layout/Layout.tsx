bd7900ab38f09fbecc09364f6a4cddf7511d4aed
keitakn <keita.koga.work@gmail.com>
2022-06-05 15:46:33 +0900

    :art: #26 言語情報を記録出来るように変更

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,24 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Footer, Props as FooterProps } from '../Footer';
-import { Header } from '../Header';
+import { Header, Props as HeaderProps } from '../Header';
 
 const Wrapper = styled.div`
   position: relative;
 `;
 
-type Props = FooterProps & {
-  children: React.ReactNode;
-};
+type Props = FooterProps &
+  HeaderProps & {
+    children: React.ReactNode;
+  };
 
 export const Layout: React.FC<Props> = ({
   terms,
   privacy,
   useNextLink,
+  language,
   children,
 }) => (
   <Wrapper>
-    <Header />
+    <Header language={language} />
     {children}
     <Footer terms={terms} privacy={privacy} useNextLink={useNextLink} />
   </Wrapper>
