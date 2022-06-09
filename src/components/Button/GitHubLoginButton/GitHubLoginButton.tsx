42866c531d477f34ade99b2d503ed6a8beae8c16
keitakn <keita.koga.work@gmail.com>
2022-06-09 18:04:56 +0900

    :recycle: #21 GitHubLoginButtonも共通のCSSを利用するように変更

--- src/components/Button/GitHubLoginButton/GitHubLoginButton.tsx
+++ src/components/Button/GitHubLoginButton/GitHubLoginButton.tsx
@@ -2,33 +2,15 @@ import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import styled from 'styled-components';
 
+import { mixins } from '../../../styles/mixins';
+
 const StyledGitHubLoginButton = styled.button`
-  display: flex;
-  flex: none;
-  flex-direction: row;
-  flex-grow: 0;
-  gap: 10px;
-  align-items: center;
-  order: 0;
-  width: 116px;
-  height: 36px;
-  padding: 4px 20px;
   background: #eb7c06;
-  border-radius: 4px;
+  ${mixins.buttonBase};
 `;
 
 const Text = styled.div`
-  flex: none;
-  flex-grow: 0;
-  order: 1;
-  width: 40px;
-  height: 16px;
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 16px;
-  color: #f2ebdf;
+  ${mixins.buttonText};
 `;
 
 const faGithubStyle = {
