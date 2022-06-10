ce6f12be27b9c9f7705245daf78a0aac2aee4d66
keitakn <keita.koga.work@gmail.com>
2022-06-10 23:36:22 +0900

    :sparkles: #21 トップページで利用するCatButtonGroupComponentを追加

---
+++ src/components/Button/CatButtonGroup/CatButtonGroup.stories.tsx
@@ -0,0 +1,12 @@
+import { CatButtonGroup } from './';
+
+import type { ComponentStoryObj, Meta } from '@storybook/react';
+
+export default {
+  title: 'src/components/Button/CatButtonGroup/CatButtonGroup.tsx',
+  component: CatButtonGroup,
+} as Meta<typeof CatButtonGroup>;
+
+type Story = ComponentStoryObj<typeof CatButtonGroup>;
+
+export const Default: Story = {};
