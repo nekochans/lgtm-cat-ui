721ef89dab5d518f1d57ce843dc3e74a4c07bf91
keitakn <keita.koga.work@gmail.com>
2022-06-26 00:50:18 +0900

    :sparkles: #40 404ページ用のTemplateComponentを仮実装

---
+++ src/components/templates/Error/NotFound/NotFound.stories.tsx
@@ -0,0 +1,16 @@
+import { NotFound } from './.';
+
+import type { ComponentStoryObj, Meta } from '@storybook/react';
+
+export default {
+  title: 'src/components/templates/NotFound/NotFound.tsx',
+  component: NotFound,
+} as Meta<typeof NotFound>;
+
+type Story = ComponentStoryObj<typeof NotFound>;
+
+export const ViewInJapanese: Story = {
+  args: {
+    language: 'ja',
+  },
+};
