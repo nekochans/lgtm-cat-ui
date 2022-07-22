12ede9eeb2eaa54fa035bd173b77031153eaa8d7
keitakn <keita.koga.work@gmail.com>
2022-07-22 17:28:12 +0900

    :sparkles: #46 アップロード成功時に表示させるButtonを追加

--- src/components/Upload/UploadModal/SuccessMessageArea.tsx
+++ src/components/Upload/UploadModal/SuccessMessageArea.tsx
@@ -89,18 +89,78 @@ const DescriptionWrapper = styled.div`
 const DescriptionText = styled.div`
   width: 359px;
   height: 40px;
-
   font-family: Roboto, sans-serif;
   font-style: normal;
   font-weight: 400;
   font-size: 11px;
   line-height: 20px;
-  /* or 182% */
-
   color: #8e7e78;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
 
-  /* Inside auto layout */
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  gap: 20px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
 
+const CloseButton = styled.button`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 20px;
+  gap: 10px;
+  background: #faf9f7;
+  border: 1px solid #8e7e78;
+  border-radius: 4px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const CloseButtonText = styled.div`
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: #f0a14e;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const MarkdownSourceCopyButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 7px 20px;
+  gap: 10px;
+  background: #eb7c06;
+  border-radius: 4px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+const MarkdownSourceCopyButtonText = styled.div`
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 18px;
+  color: #ffffff;
   flex: none;
   order: 0;
   flex-grow: 0;
@@ -112,7 +172,7 @@ export const SuccessMessageArea: React.FC = () => (
     <ContentsWrapper>
       <MainMessage>
         LGTM画像を作成しているので少々お待ち下さい。
-        「Markdownソースをコピー」ボタンか以下の画像を
+        「Markdownソースをコピー」ボタンか上の画像を
         クリックするとMarkdownソースがコピーされます。
       </MainMessage>
       <UnderSectionWrapper>
@@ -121,6 +181,16 @@ export const SuccessMessageArea: React.FC = () => (
             ※トップページの「新着の猫ちゃんを表示」からもアップロードした画像を確認できます。
           </DescriptionText>
         </DescriptionWrapper>
+        <ButtonGroup>
+          <CloseButton>
+            <CloseButtonText>閉じる</CloseButtonText>
+          </CloseButton>
+          <MarkdownSourceCopyButton>
+            <MarkdownSourceCopyButtonText>
+              Markdownソースをコピー
+            </MarkdownSourceCopyButtonText>
+          </MarkdownSourceCopyButton>
+        </ButtonGroup>
       </UnderSectionWrapper>
     </ContentsWrapper>
   </Wrapper>
