3b5bf201f1408647f4af1a2799ab175a486c95fd
keitakn <keita.koga.work@gmail.com>
2022-07-22 12:06:50 +0900

    :sparkles: #46 Prospで受け取る関数のIFを統一する為にResult型を定義

---
+++ src/features/__tests__/result/createSuccessResult.spec.ts
@@ -0,0 +1,32 @@
+import {createSuccessResult, isSuccessResult} from '../../result';
+
+describe('src/features/result.ts createSuccessResult TestCase', () => {
+  type Email = `${string}@${string}`;
+
+  type User = {
+    email: Email;
+    name: string;
+  };
+
+  const isEmail = (value: unknown): value is Email => {
+    if (typeof value !== 'string') {
+      return false;
+    }
+
+    return value.includes('@');
+  };
+
+  it('should return a return type', () => {
+    const user = {
+      email: 'moko-cat@exmple.com',
+      name: 'Mkoko',
+    } as const;
+
+    const result = createSuccessResult<User>(user);
+
+    expect(isSuccessResult(result)).toBeTruthy();
+    expect(isEmail(result.value.email)).toBeTruthy();
+    expect(result.value.email).toStrictEqual(user.email);
+    expect(result.value.name).toStrictEqual(user.name);
+  });
+});
