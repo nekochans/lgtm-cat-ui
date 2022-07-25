e3eb974858992b89e462eeae84e8ccf1425a1d53
keitakn <keita.koga.work@gmail.com>
2022-07-25 16:22:43 +0900

    :shirt: #46 ESLintのエラー（ファイルが大きすぎる）を解消する為にリファクタリング

--- src/components/Upload/UploadForm/UploadForm.tsx
+++ src/components/Upload/UploadForm/UploadForm.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
-import React, { useState, type FC, ChangeEvent } from 'react';
+import React, { ChangeEvent, type FC, useState } from 'react';
 import { FaCloudUploadAlt } from 'react-icons/fa';
-import styled from 'styled-components';
 
 import { isValidFileType } from '../../../features/lgtmImage';
 import { createLinksFromLanguages } from '../../../features/privacyPolicy';
@@ -17,36 +16,31 @@ import { UploadErrorMessageArea } from '../UploadErrorMessageArea';
 import { UploadModal } from '../UploadModal';
 import { UploadTitleArea } from '../UploadTitleArea';
 
+import {
+  CautionTextArea,
+  DescriptionAreaWrapper,
+  Form,
+  InputFile,
+  InputFileArea,
+  InputFileLabel,
+  InputFileLabelText,
+  MaxUploadSizeText,
+  Notes,
+  PrivacyLinkText,
+  PrivacyPolicyArea,
+  Text,
+  UploadButtonWrapper,
+  Wrapper,
+} from './StyledComponents';
+import {
+  cautionText,
+  createNotAllowedImageExtensionErrorMessage,
+  imageDropAreaText,
+  noteList,
+  uploadInputButtonText,
+} from './i18n';
+
 import type { Language } from '../../../types/language';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex: none;
-  flex-direction: column;
-  flex-grow: 0;
-  gap: 61px;
-  align-items: center;
-  order: 0;
-  padding: 0;
-`;
-
-const Form = styled.form`
-  @media (max-width: 767px) {
-    width: 380px;
-  }
-  flex: none;
-  flex-grow: 0;
-  order: 1;
-  width: 500px;
-`;
-
-const InputFileArea = styled.div`
-  box-sizing: border-box;
-  display: grid;
-  height: 220px;
-  background: #fff;
-  border: 1px dashed #362e2b;
-`;
 
 const faCloudUploadAltStyle = {
   fontStyle: 'normal',
@@ -61,177 +55,7 @@ const faCloudUploadAltStyle = {
   margin: 'auto',
 };
 
-const Text = styled.div`
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 28px;
-  color: #8e7e78;
-  text-align: center;
-`;
-
-const InputFile = styled.input`
-  display: none;
-`;
-
-const InputFileLabel = styled.label`
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
-  height: 43px;
-  padding: 12px 20px;
-  margin: auto;
-  cursor: pointer;
-  background: #faf9f7;
-  border: 1px solid #8e7e78;
-  border-radius: 4px;
-`;
-
-const InputFileLabelText = styled.div`
-  flex: none;
-  flex-grow: 0;
-  order: 0;
-  width: 160px;
-  height: 19px;
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 19px;
-  color: #f0a14e;
-`;
-
-const MaxUploadSizeText = styled.div`
-  height: 28px;
-  font-family: Roboto, sans-serif;
-  font-size: 13px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 28px;
-  color: #8e7e78;
-  text-align: right;
-`;
-
-const CautionTextArea = styled.div`
-  height: 28px;
-  font-family: Roboto, sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 28px;
-  color: #362e2b;
-  text-align: center;
-`;
-
-const Notes = styled.div`
-  top: 609px;
-  height: 96px;
-  font-family: Roboto, sans-serif;
-  font-size: 11px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px;
-  color: #362e2b;
-`;
-
-const DescriptionAreaWrapper = styled.div`
-  display: grid;
-  gap: 24px;
-`;
-
-const PrivacyPolicyArea = styled.div`
-  height: 42px;
-  font-family: Roboto, sans-serif;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 21px;
-  color: #362e2b;
-  text-align: center;
-`;
-
-const PrivacyLinkText = styled.a`
-  height: 42px;
-  font-family: Roboto, sans-serif;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 21px;
-  color: #eb7c06;
-  text-align: center;
-  text-decoration-line: underline;
-  cursor: pointer;
-`;
-
-const UploadButtonWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const imageDropAreaText = (language: Language): string => {
-  switch (language) {
-    case 'ja':
-      return 'ここに画像をドロップ';
-    case 'en':
-      return 'Drop image here';
-    default:
-      return assertNever(language);
-  }
-};
-
-const uploadInputButtonText = (language: Language): string => {
-  switch (language) {
-    case 'ja':
-      return 'またはファイルの選択';
-    case 'en':
-      return 'Select an image file';
-    default:
-      return assertNever(language);
-  }
-};
-
-const cautionText = (language: Language): string => {
-  switch (language) {
-    case 'ja':
-      return '注意事項';
-    case 'en':
-      return 'precautions';
-    default:
-      return assertNever(language);
-  }
-};
-
-const noteList = (language: Language): string[] => {
-  switch (language) {
-    case 'ja':
-      return [
-        '拡張子が png, jpg, jpeg の画像のみアップロード出来ます。',
-        '猫が写っていない画像はアップロード出来ません。',
-        '人の顔がはっきり写っている画像はアップロード出来ません。',
-        '猫のイラスト等は正確に判定出来ない事があります。',
-      ];
-    case 'en':
-      return [
-        'png, jpg, jpeg, images are available.',
-        'Images without cats cannot be uploaded.',
-        "Images that clearly show a person's face cannot be uploaded.",
-        'Illustrations of cats may not be accurately determined.',
-      ];
-    default:
-      return assertNever(language);
-  }
-};
-
-const createPrivacyPolicyArea = (language: Language): JSX.Element => {
+export const createPrivacyPolicyArea = (language: Language): JSX.Element => {
   const privacyLinkAttribute = createLinksFromLanguages(language);
 
   switch (language) {
@@ -255,26 +79,6 @@ const createPrivacyPolicyArea = (language: Language): JSX.Element => {
           .
         </PrivacyPolicyArea>
       );
-    default:
-      return assertNever(language);
-  }
-};
-
-const createNotAllowedImageExtensionErrorMessage = (
-  fileType: string,
-  language: Language,
-): string[] => {
-  switch (language) {
-    case 'ja':
-      return [
-        `${fileType} の画像は許可されていません。`,
-        'png, jpg, jpeg の画像のみアップロード出来ます。',
-      ];
-    case 'en':
-      return [
-        `${fileType} is not allowed.`,
-        'Only png, jpg, jpeg images can be uploaded.',
-      ];
     default:
       return assertNever(language);
   }
