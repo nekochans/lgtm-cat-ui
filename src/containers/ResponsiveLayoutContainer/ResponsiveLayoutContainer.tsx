77adae4b2c5643a3055bcfe3f634dded64d16b27
keitakn <keita.koga.work@gmail.com>
2022-08-01 18:09:30 +0900

    :recycle: #76 言語切替の関数群は様々な箇所で利用するのでカスタムフック化した

--- src/containers/ResponsiveLayoutContainer/ResponsiveLayoutContainer.tsx
+++ src/containers/ResponsiveLayoutContainer/ResponsiveLayoutContainer.tsx
@@ -1,52 +1,26 @@
-import { useSnapshot } from 'valtio';
-
+import { useSwitchLanguage } from '../../hooks/useSwitchLanguage';
 import { ResponsiveLayout } from '../../layouts';
-import {
-  headerStateSelector,
-  updateIsLanguageMenuDisplayed,
-  updateLanguage,
-} from '../../stores/valtio/common';
 
 import type { Language } from '../../types/language';
-import type { FC, ReactNode, MouseEvent } from 'react';
+import type { FC, ReactNode } from 'react';
 
 type Props = {
   language: Language;
   children: ReactNode;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const onClickEn = (event: MouseEvent<HTMLDivElement>) => {
-  updateLanguage('en');
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const onClickJa = (event: MouseEvent<HTMLDivElement>) => {
-  updateLanguage('ja');
-};
-
 export const ResponsiveLayoutContainer: FC<Props> = ({
   language,
   children,
 }) => {
-  const snap = useSnapshot(headerStateSelector());
-
-  const { isLanguageMenuDisplayed } = snap;
-
-  const selectedLanguage = snap.language ? snap.language : language;
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const onClickLanguageButton = (event: MouseEvent<HTMLDivElement>) => {
-    updateIsLanguageMenuDisplayed(!isLanguageMenuDisplayed);
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const onClickOutSideMenu = (event: MouseEvent<HTMLDivElement>) => {
-    // メニューの外側をクリックしたときだけメニューを閉じる
-    if (isLanguageMenuDisplayed) {
-      updateIsLanguageMenuDisplayed(false);
-    }
-  };
+  const {
+    isLanguageMenuDisplayed,
+    selectedLanguage,
+    onClickEn,
+    onClickJa,
+    onClickLanguageButton,
+    onClickOutSideMenu,
+  } = useSwitchLanguage(language);
 
   return (
     <div onClick={onClickOutSideMenu} aria-hidden="true">
---
+++ src/hooks/useSwitchLanguage.ts
@@ -0,0 +1,49 @@
+import { MouseEvent } from 'react';
+import { useSnapshot } from 'valtio';
+
+import {
+  headerStateSelector,
+  updateIsLanguageMenuDisplayed,
+  updateLanguage,
+} from '../stores/valtio/common';
+import { Language } from '../types/language';
+
+export const useSwitchLanguage = (language: Language) => {
+  const snap = useSnapshot(headerStateSelector());
+
+  const { isLanguageMenuDisplayed } = snap;
+
+  const selectedLanguage = snap.language ? snap.language : language;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onClickEn = (event: MouseEvent<HTMLDivElement>) => {
+    updateLanguage('en');
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onClickJa = (event: MouseEvent<HTMLDivElement>) => {
+    updateLanguage('ja');
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onClickLanguageButton = (event: MouseEvent<HTMLDivElement>) => {
+    updateIsLanguageMenuDisplayed(!isLanguageMenuDisplayed);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onClickOutSideMenu = (event: MouseEvent<HTMLDivElement>) => {
+    // メニューの外側をクリックしたときだけメニューを閉じる
+    if (isLanguageMenuDisplayed) {
+      updateIsLanguageMenuDisplayed(false);
+    }
+  };
+
+  return {
+    isLanguageMenuDisplayed,
+    selectedLanguage,
+    onClickEn,
+    onClickJa,
+    onClickLanguageButton,
+    onClickOutSideMenu,
+  };
+};
