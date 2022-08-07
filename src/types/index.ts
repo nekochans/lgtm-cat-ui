4e276d23bf07112daf060c5a3c33999c0118ea66
keitakn <keita.koga.work@gmail.com>
2022-08-07 14:17:34 +0900

    :sparkles: #118 外でも利用する型定義をexportするように変更

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export { Language } from './language';
+export {
+  LgtmImageUrl,
+  LgtmImage,
+  AcceptedTypesImageExtension,
+  ImageValidator,
+  ImageUploader,
+  CatImagesFetcher,
+} from './lgtmImage';
