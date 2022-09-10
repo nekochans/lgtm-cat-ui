de369b0be70f18de59b4c45282400ec9b91d1f0a
keitakn <keita.koga.work@gmail.com>
2022-09-10 23:26:42 +0900

    :sparkles: #153 GTM用のカスタムデータ属性型を追加

---
+++ src/types/gtm.ts
@@ -0,0 +1,8 @@
+export type CustomDataAttrGtmClick =
+  | 'top-upload-cat-button'
+  | 'top-fetch-random-cat-button'
+  | 'top-fetch-new-arrival-cat-button'
+  | 'language-menu-ja-button'
+  | 'language-menu-en-button'
+  | 'footer-terms-link'
+  | 'footer-privacy-link';
