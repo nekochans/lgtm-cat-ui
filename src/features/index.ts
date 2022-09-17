09d66e78f20e63f7ab1b8d1f06b2f55c1eea3159
keitakn <keita.koga.work@gmail.com>
2022-09-17 21:44:25 +0900

    :recycle: #163 外のディレクトリに公開している関数はindex.tsに追加

--- src/features/index.ts
+++ src/features/index.ts
@@ -9,3 +9,9 @@ export {
 } from './result';
 
 export { errorType, type ErrorType } from './error';
+
+export { isValidFileType, extractImageExtFromValidFileType } from './lgtmImage';
+
+export { createPrivacyPolicyLinksFromLanguages } from './privacyPolicy';
+
+export { createTermsOfUseLinksFromLanguages } from './termsOfUse';
