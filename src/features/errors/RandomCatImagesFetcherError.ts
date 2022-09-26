8363d1e6de633d367a84ae0c0a6d0976244e3995
keitakn <keita.koga.work@gmail.com>
2022-09-26 21:30:37 +0900

    :shirt: #185 以下の記述があるので念の為、if文を追加しておきたいので ESLint を無効化
    >https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Error
    >標準外の V8 の関数で、 Error インスタンスに stack プロパティを生成します。

--- src/features/errors/RandomCatImagesFetcherError.ts
+++ src/features/errors/RandomCatImagesFetcherError.ts
@@ -3,6 +3,10 @@ export class RandomCatImagesFetcherError extends Error {
     super(error);
     this.name = new.target.name;
 
+    // 以下の記述があるので念の為、if文を追加しておきたいので @typescript-eslint/strict-boolean-expressions を無効化
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Error
+    // >標準外の V8 の関数で、 Error インスタンスに stack プロパティを生成します。
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
