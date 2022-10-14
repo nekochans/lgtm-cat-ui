2185826c401b89a417c935b21df99b962d7d713e
keitakn <keita.koga.work@gmail.com>
2022-10-14 22:28:39 +0900

    :wrench: #182 Vite関連のpackageを追加しViteでBuildする設定を追加

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'lgtm-cat-ui',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'styled-components',
+        'next/link',
+        'next/image',
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+          'next/link': 'Link',
+          'next/image': 'Image',
+        },
+      },
+    },
+  },
+});
