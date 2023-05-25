fe31f05f4cd3dd200f0939f9584086f0018db916
keitakn <keita.koga.work@gmail.com>
2023-05-25 11:05:10 +0900

    :recycle: #271 シンボリックを使う方法よりも `storybook-css-modules` を利用したほうが良さそうなので変更

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import 'ress/ress.css';
 import './markdown.css';
-import './globals.css';
+import '../src/styles/globals.css';
 import type { Preview } from '@storybook/react';
 
 const customViewports = {
