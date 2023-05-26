fb20ea75c8e048a72fdbd40c7282a0e77b67e7fc
keitakn <keita.koga.work@gmail.com>
2023-05-26 19:03:45 +0900

    :recycle: #276 LanguageButton をCSS Modulesを使った形に置換え

--- src/components/Header/LanguageButton.tsx
+++ src/components/Header/LanguageButton.tsx
@@ -1,50 +1,16 @@
 import type { FC, MouseEvent } from 'react';
 import { FaCaretDown } from 'react-icons/fa';
-import styled from 'styled-components';
-import { mixins } from '../../styles';
-
-const _Wrapper = styled.div`
-  @media (max-width: ${mixins.mediaQuerySize.default}) {
-    padding: 12px 0;
-  }
-  box-sizing: border-box;
-  display: flex;
-  flex: none;
-  flex-direction: row;
-  flex-grow: 0;
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
-  order: 1;
-  padding: 12px 20px;
-  border-radius: 4px;
-`;
-
-const _Text = styled.p`
-  flex: none;
-  flex-grow: 0;
-  order: 0;
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 19px;
-  color: ${mixins.colors.primary};
-  cursor: pointer;
-`;
-
-const faCaretDownStyle = {
-  color: `${mixins.colors.primary}`,
-  cursor: 'pointer',
-};
+import styles from './LanguageButton.module.css';
 
 type Props = {
   onClick: (event: MouseEvent<HTMLDivElement>) => void;
 };
 
 export const LanguageButton: FC<Props> = ({ onClick }) => (
-  <_Wrapper onClick={onClick}>
-    <_Text>Language</_Text>
-    <FaCaretDown style={faCaretDownStyle} />
-  </_Wrapper>
+  // TODO 今はESLintのエラーを無視しているが https://github.com/nekochans/lgtm-cat-ui/issues/279 で修正する
+  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+  <div className={styles.wrapper} onClick={onClick}>
+    <p className={styles.text}>Language</p>
+    <FaCaretDown className={styles['fa-caret-down']} />
+  </div>
 );
