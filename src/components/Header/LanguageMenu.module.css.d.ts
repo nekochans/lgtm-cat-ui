7d51ab006bc6385d9bdc29ecffe572d3e3af45fd
keitakn <keita.koga.work@gmail.com>
2023-05-26 16:14:54 +0900

    :recycle: #276 LanguageMenu をCSS Moduleを使った形に置換え

---
+++ src/components/Header/LanguageMenu.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly wrapper: string;
+  readonly 'en-text-wrapper': string;
+  readonly 'ja-text-wrapper': string;
+  readonly 'link-text': string;
+  readonly link: string;
+  readonly separator: string;
+};
+export = styles;
