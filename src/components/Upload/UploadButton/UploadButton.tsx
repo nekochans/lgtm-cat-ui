9261ceb8779ebfe894a251fd23ae192c7035ad2d
keitakn <keita.koga.work@gmail.com>
2023-05-29 16:30:53 +0900

    :recycle: #289 UploadButton をCSSModules に置換え

--- src/components/Upload/UploadButton/UploadButton.tsx
+++ src/components/Upload/UploadButton/UploadButton.tsx
@@ -1,63 +1,7 @@
 import type { FC, FormEventHandler } from 'react';
-import styled, { css } from 'styled-components';
-import { mixins } from '../../../styles';
 import type { Language } from '../../../types';
 import { assertNever } from '../../../utils';
-
-const buttonCss = css`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
-  padding: 8px 20px;
-  background: ${mixins.colors.primary};
-  border-radius: 4px;
-`;
-
-const hoverCss = css`
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-const _Button = styled.button`
-  ${buttonCss};
-  ${hoverCss};
-`;
-
-const buttonTextCss = css`
-  flex: none;
-  flex-grow: 0;
-  order: 0;
-  width: 128px;
-  height: 18px;
-  font-family: Roboto, sans-serif;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 18px;
-  color: ${mixins.colors.white};
-`;
-
-const _Text = styled.div`
-  ${buttonTextCss};
-  ${hoverCss};
-`;
-
-const disableCss = css`
-  opacity: 0.5;
-`;
-
-const _DisableButton = styled.button`
-  ${buttonCss};
-  ${disableCss};
-`;
-
-const _DisableText = styled.div`
-  ${buttonTextCss};
-  ${disableCss};
-`;
+import styles from './UploadButton.module.css';
 
 const createText = (language: Language): string => {
   switch (language) {
@@ -79,15 +23,27 @@ type Props = {
 export const UploadButton: FC<Props> = ({ language, disabled, onClick }) => {
   if (!disabled) {
     return (
-      <_Button type="submit" disabled={disabled} onClick={onClick}>
-        <_Text>{createText(language)}</_Text>
-      </_Button>
+      <button
+        type="submit"
+        disabled={disabled}
+        onClick={onClick}
+        className={styles.button}
+      >
+        <div className={styles.text}>{createText(language)}</div>
+      </button>
     );
   }
 
   return (
-    <_DisableButton type="submit" disabled={disabled} onClick={onClick}>
-      <_DisableText>{createText(language)}</_DisableText>
-    </_DisableButton>
+    <button
+      type="submit"
+      disabled={disabled}
+      onClick={onClick}
+      className={`${styles.button} ${styles['disabled-button']}`}
+    >
+      <div className={`${styles.text} ${styles['disabled-text']}`}>
+        {createText(language)}
+      </div>
+    </button>
   );
 };
