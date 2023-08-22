17bcfd4ce01ecef0394ad9589ff5734b2c5b1249
keitakn <keita.koga.work@gmail.com>
2023-08-22 17:34:30 +0900

    :heavy_minus_sign: #315 storybook-css-modules は既に不要になっているので削除

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,6 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
-    'storybook-css-modules',
   ],
   framework: {
     name: '@storybook/nextjs',
